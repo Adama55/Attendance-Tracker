e02ab4866a327e28e2d65824c91fbb5efcf29086
Adama55 <mafahfof55@gmail.com>
2023-10-17 22:56:47 +0200

    router for student

--- routers/router_students.py
+++ routers/router_students.py
@@ -8,3 +8,36 @@ router= APIRouter(
     prefix='/students',
     tags=["Students"]
 )
+
+students = [
+    Student(id="student1", name="Adama"),
+    Student(id="student2", name="Adrien"),
+    Student(id="student3", name="Akbar")
+]
+
+@router.get('', response_model=List[Student])
+async def get_student():
+    """List all the students from a Training Center (context fonctionnel ou technique)"""
+    return students
+
+@router.get('/{student_id}', response_model=Student)
+async def get_student_by_id(student_id: str):
+    for student in students:
+        if student.id == student_id:
+            return student
+    raise HTTPException(status_code=404, detail="Student not found")
+
+@router.post('', response_model=Student, status_code=201)
+async def add_new_student(giveName:StudentNoID):
+    generatedId= uuid.uuid4()
+    newStudent= Student(id=str(generatedId), name=giveName.name)
+    students.append(newStudent)
+    return 
+
+@router.patch('/{student_id}', status_code=204)
+async def modify_student_name(student_id:str, modifiedStudent: StudentNoID):
+    for student in students:
+        if student.id == student_id:
+            student.name=modifiedStudent.name
+            return student
+    raise HTTPException(status_code= 404, detail="Student not found")
