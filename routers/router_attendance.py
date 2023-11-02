4d92f55bded7c49e5587da25e5d3415b406aaef9
Adama55 <mafahfof55@gmail.com>
2023-11-02 23:12:27 +0100

    ajout get attendance

--- routers/router_attendance.py
+++ routers/router_attendance.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter, HTTPException
+from typing import List
 
 
 router= APIRouter(
     prefix='/attendance',
     tags=["attendance"]
 )
+@router.get('', response_model=List)
+async def get_attendance():
+    """List all the courses."""
+    return { "message" :"all attendance"}
